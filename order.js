bb8510e815e05deabdab83e7347d7bf08a3cf46e
HarveyThePooka404 <57190763+HarveyThePooka404@users.noreply.github.com>
2020-06-01 01:15:46 +0200

    order always

---
+++ order.js
@@ -0,0 +1,54 @@
+let orderNav = document.querySelectorAll(".ordernav");
+console.log(orderNav);
+
+function startOrder(type) {
+    if (type == "personal") {
+        orderNav[0].style.backgroundColor = "#273440";
+        document.getElementById("order-section0").style.display = "none";
+        document.getElementById("order-section1").style.display = "block";
+    } else if (type == "collab") {
+        orderNav[0].style.backgroundColor = "#273440";
+        document.getElementById("order-section0").style.display = "none";
+        document.getElementById("order-section1").style.display = "block";
+    } else if (type == "staff") {
+        orderNav[0].style.backgroundColor = "#273440";
+        document.getElementById("order-section0").style.display = "none";
+        document.getElementById("order-section1").style.display = "block";
+    } else if (type == "other") {
+        orderNav[0].style.backgroundColor = "#273440";
+        document.getElementById("order-section0").style.display = "none";
+        document.getElementById("order-section1").style.display = "block";
+    }
+}
+
+document.getElementById("order-next1").addEventListener("click", function () {
+    document.getElementById("order-section1").style.display = "none";
+    document.getElementById("order-section2").style.display = "block";
+    orderNav[1].style.backgroundColor = "#273440";
+    console.log("AAAAA");
+});
+
+document.getElementById("order-next2").addEventListener("click", function () {
+    document.getElementById("order-section2").style.display = "none";
+    document.getElementById("order-section3").style.display = "block";
+    orderNav[2].style.backgroundColor = "#273440";
+})
+
+document.getElementById("orderform").addEventListener("submit", function () {
+    orderNav[3].style.backgroundColor = "#273440";
+    document.getElementById("order-section3").style.display = "none";
+    document.getElementById("order-section4").style.display = "block";
+
+    getFormData();
+});
+
+function getFormData(){
+    var elements = document.getElementById("orderform").elements;
+    var obj ={};
+    for(var i = 0 ; i < elements.length ; i++){
+        var item = elements.item(i);
+        obj[item.name] = item.value;
+    }
+
+    console.log(JSON.stringify(obj));
+}
